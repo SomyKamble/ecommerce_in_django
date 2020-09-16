1425983428ab5350b8854d267360d0fbc82bd243
SomyKamble <somykamble1001@gmail.com>
2020-09-16 21:42:28 +0530

    by somy

---
+++ store/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from .models import *
+
+# Create your views here.
+def store(request):
+    products = Product.objects.all()
+    context={
+        'products' :products
+    }
+
+    return render(request,'store.html',context)
+
+def cart(request):
+    context={}
+
+    return render(request,'cart.html',context)
+
+
+def checkout(request):
+    context={}
+
+    return render(request,'checkout.html',context)
